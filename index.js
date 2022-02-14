9eef43f6097248790f82f5f63aadf21440d24ab6
candacehernandez <chernandez.aggie@gmail.com>
2022-02-14 15:04:36 -0600

    commit tree 69a96fd14e0869d6f8928fa5a5fc995d22533cca

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+/*function findMatching(list, match){
+   let newArray = list.filter(function(driverName){
+       return driverName === match;
+   })
+   return newArray;
+}*/
+
+function findMatching(list, match){
+    return list.filter(element => element.toUpperCase() === match.toUpperCase());
+}
+
+function fuzzyMatch(list, match){
+    return list.filter(element => element.charAt(0) === match.charAt(0));
+}
+
+function matchName(list, match){
+    return list.filter(element => element.name === match);
+}
